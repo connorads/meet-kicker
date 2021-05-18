f30e6251708a734b5922aabba899cf17ff5c065e
Connor Adams <connorads@users.noreply.github.com>
2021-05-18 21:27:33 +0100

    Add error messages

--- src/content.js
+++ src/content.js
@@ -1,6 +1,30 @@
+const getAlertText = (errorMessage) => `Meet Kicker 🦶
+
+${errorMessage}
+
+• Is there someone else in the meeting?
+• Do you have permission to remove users?
+
+If both yes but still not working please report this issue
+https://github.com/connorads/meet-kicker/issues`;
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.text === "kick") {
-    document.querySelector('[data-tooltip="Remove from meeting"]')?.click();
-    setTimeout(() => document.querySelector('[data-id="EBS5u"]')?.click(), 100);
+    const removeButton = document.querySelector(
+      '[data-tooltip="Remove from meeting"]'
+    );
+    if (removeButton) {
+      removeButton.click();
+      setTimeout(() => {
+        const confirmButton = document.querySelector('[data-id="EBS5u"]');
+        if (confirmButton) {
+          confirmButton.click();
+        } else {
+          alert(getAlertText("Error 2: Confirm button not found 😢"));
+        }
+      }, 100);
+    } else {
+      alert(getAlertText("Error 1: Remove user button not found 😢"));
+    }
   }
 });
