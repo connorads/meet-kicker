8c88165ec63ecc46c71f371eb8ee75f7a92312f9
Connor Adams <connorads@users.noreply.github.com>
2021-05-18 21:02:53 +0100

    crazy refactoring

--- src/background.js
+++ src/background.js
@@ -2,6 +2,6 @@ chrome.tabs.onActivated.addListener(({ tabId }) => {
   chrome.pageAction.show(tabId);
 });
 
-chrome.pageAction.onClicked.addListener(function (tab) {
+chrome.pageAction.onClicked.addListener((tab) => {
   chrome.tabs.sendMessage(tab.id, { text: "kick" });
 });
