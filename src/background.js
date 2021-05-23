630ad7a415332ed34d42c5aa78b8f3119409ff22
Connor Adams <connorads@users.noreply.github.com>
2021-05-23 14:57:41 +0100

    Listen to tab updates and show page action

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
-const showPageAction = ({ tabId }) => chrome.pageAction.show(tabId);
-
-chrome.tabs.onCreated.addListener(showPageAction);
-chrome.tabs.onActivated.addListener(showPageAction);
+const showPageAction = (tabId) => chrome.pageAction.show(tabId);
+chrome.tabs.onCreated.addListener(({ tabId }) => showPageAction(tabId));
+chrome.tabs.onActivated.addListener(({ tabId }) => showPageAction(tabId));
+chrome.tabs.onUpdated.addListener(showPageAction);
 
 chrome.pageAction.onClicked.addListener((tab) =>
   chrome.tabs.sendMessage(tab.id, { text: "kick" })
