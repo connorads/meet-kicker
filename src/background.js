7b70e75599fef08bb1b76e58a8559963821143c3
Connor Adams <connorads@users.noreply.github.com>
2021-05-23 15:40:31 +0100

    Use declarativeContent to only show page action on Google Meet

--- src/background.js
+++ src/background.js
@@ -1,7 +1,19 @@
-const showPageAction = (tabId) => chrome.pageAction.show(tabId);
-chrome.tabs.onCreated.addListener(({ tabId }) => showPageAction(tabId));
-chrome.tabs.onActivated.addListener(({ tabId }) => showPageAction(tabId));
-chrome.tabs.onUpdated.addListener(showPageAction);
+const showPageActionOnGoogleMeet = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { hostEquals: "meet.google.com", schemes: ["https"] },
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()],
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      showPageActionOnGoogleMeet,
+    ]);
+  });
+});
 
 chrome.pageAction.onClicked.addListener((tab) =>
   chrome.tabs.sendMessage(tab.id, { text: "kick" })
